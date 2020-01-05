d807714efb92aa66ba9ec3c7c42ca909a4636d4a
Chris Lieb <clieb@bitsighttech.com>
2020-01-05 17:13:12 -0500

    Add solutino for 01-01

--- src/bin/day01-prob01.rs
+++ src/bin/day01-prob01.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    let fuels = std::fs::read_to_string("src/bin/day01.txt")
+        .map(|file| {
+            file.split("\n")
+                .filter(|line| line.len() > 0)
+                .map(|val| val.parse::<i32>().map(fuel_for).ok().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .expect("Unable to open file");
+    println!("{}", fuels.iter().sum::<i32>());
+}
+
+fn fuel_for(mass: i32) -> i32 {
+    mass / 3 - 2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(fuel_for(12), 2);
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(fuel_for(14), 2);
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(fuel_for(1969), 654);
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!(fuel_for(100756), 33583);
+    }
 }
